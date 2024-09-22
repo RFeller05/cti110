c5f2c054989706cbe67d6b8a8dd568c500fc8bcc
RFeller05 <fellerr0472@student.faytechcc.edu>
2024-09-22 12:42:45 -0400

    Add files via upload
    First submission.

---
+++ P1LAB1_FellerRobert.py
@@ -0,0 +1,15 @@
+# Robert Feller
+# 9/22/2024
+# P1LAB1
+# Prompts the user to enter their first and last name, then displays it in a welcome message
+
+firstName = ''
+lastName = ''
+
+print('Enter your first name:', end=' ')
+firstName = input()
+
+print('Enter your last name:', end=' ')
+lastName = input()
+
+print('Hello,', firstName, lastName, 'and welcome to CTI-110')
